25e760fa5d096631e7bb8d956e77034509c5c421
Onduso <londuso@us.ci.org>
2020-07-28 23:06:08 +0300

    Implemented restricted datepicker to only allow the 1st day of the month

--- reports/views/backend/admin/covid19_report.php
+++ reports/views/backend/admin/covid19_report.php
@@ -14,14 +14,14 @@
       <div class='col-xs-6'>
         <select class='form-control' id='report_grouping'>
           <option value=''><?= get_phrase('select_grouping'); ?></option>
-          <option value='beneficiary' <?php if($group_report_by == 'beneficiary') echo 'selected';?> ><?= get_phrase('beneficiary_count'); ?></option>
-          <option value='household' <?php if($group_report_by == 'household') echo 'selected';?> ><?= get_phrase('household_count'); ?></option>
-          <option value='fcp' <?php if($group_report_by == 'fcp') echo 'selected';?> ><?= get_phrase('fcp_count'); ?></option>
-          <option value='amount' <?php if($group_report_by == 'amount') echo 'selected';?> ><?= get_phrase('amount_spent'); ?></option>
+          <option value='beneficiary' <?php if ($group_report_by == 'beneficiary') echo 'selected'; ?>><?= get_phrase('beneficiary_count'); ?></option>
+          <option value='household' <?php if ($group_report_by == 'household') echo 'selected'; ?>><?= get_phrase('household_count'); ?></option>
+          <option value='fcp' <?php if ($group_report_by == 'fcp') echo 'selected'; ?>><?= get_phrase('fcp_count'); ?></option>
+          <option value='amount' <?php if ($group_report_by == 'amount') echo 'selected'; ?>><?= get_phrase('amount_spent'); ?></option>
         </select>
       </div>
       <div class='col-xs-2'>
-        <input type='text' class='form-control datepicker' id='reporting_month' data-format="yyyy-mm-dd"  />
+        <input type='text' class='form-control datepicker' id='reporting_month' data-format="yyyy-mm-dd" />
       </div>
       <div class='col-xs-2'>
         <button class='btn btn-primary' id='load_report'><?= get_phrase('load_report'); ?></button>
@@ -37,7 +37,7 @@
 <div class='row'>
   <div class='col-xs-12' id='report_holder'>
     <?php
-      include "includes/include_covid19_report.php";
+    include "includes/include_covid19_report.php";
     ?>
   </div>
 
@@ -66,7 +66,7 @@
   //     //Display the total of all td to last td
   //     $(tr).find('td').last().html(total.toFixed(2));
   //     $(tr).find('td:last-child').css({backgroundColor:'gray',color:'white'});
-      
+
 
   //   });
 
@@ -94,65 +94,88 @@
 
   // }
 
-  $("#load_report").on('click',function(){
+  $("#load_report").on('click', function() {
     var report_grouping = $("#report_grouping").val();
     var reporting_month = $("#reporting_month").val();
 
-    if(report_grouping !== ""){
-      var url = "<?=base_url();?>reports.php/admin/covid19_report";
-      var data = {'group_name':report_grouping,'reporting_month':reporting_month};
+    if (report_grouping !== "") {
+      var url = "<?= base_url(); ?>reports.php/admin/covid19_report";
+      var data = {
+        'group_name': report_grouping,
+        'reporting_month': reporting_month
+      };
 
-      $.post(url,data,function(response){
+      $.post(url, data, function(response) {
         $('#report_holder').html(response);
       });
     }
-    
-  });
 
+  });
 </script>
 
 
 <style>
-#overlay{
-    position: fixed; /* Sit on top of the page content */
-    display: none; /* Hidden by default */
-    width: 100%; /* Full width (cover the whole page) */
-    height: 100%; /* Full height (cover the whole page) */
+  #overlay {
+    position: fixed;
+    /* Sit on top of the page content */
+    display: none;
+    /* Hidden by default */
+    width: 100%;
+    /* Full width (cover the whole page) */
+    height: 100%;
+    /* Full height (cover the whole page) */
     top: 0;
     left: 0;
     right: 0;
     bottom: 0;
-    background-color: rgba(0,0,0,0.5); /* Black background with opacity */
-    z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
-    cursor: pointer; /* Add a pointer on hover */
-}
+    background-color: rgba(0, 0, 0, 0.5);
+    /* Black background with opacity */
+    z-index: 2;
+    /* Specify a stack order in case you're using a different order for other elements */
+    cursor: pointer;
+    /* Add a pointer on hover */
+  }
 
-#overlay img {
+  #overlay img {
     position: absolute;
     top: 50%;
     left: 50%;
-}
+  }
 </style>
 
-<div id="overlay"><img src='<?php echo base_url()."uploads/preloader4.gif";?>'/></div>
+<div id="overlay"><img src='<?php echo base_url() . "uploads/preloader4.gif"; ?>' /></div>
 
 <script>
-$( document ).ajaxSend(function() {
-  $("#overlay").css("display","block");
-});
+  $(document).ajaxSend(function() {
+    $("#overlay").css("display", "block");
+  });
 
-$(document).ajaxSuccess(function() {
-    $("#overlay").css("display","none");
-});
+  $(document).ajaxSuccess(function() {
+    $("#overlay").css("display", "none");
+  });
 
-$(document).ajaxError(function(xhr) {
+  $(document).ajaxError(function(xhr) {
     alert('Error has occurred');
-});
+  });
 
-$(document).ready(function(){
-  $('.datepicker').datepicker({
-			format: 'yyyy-mm-dd'
-		});
-});
+  //Restrict Date selection to 1st day of the month
 
+  $(document).ready(function() {
+    $('.datepicker').datepicker({
+      beforeShowDay: function(d) {
+        if (d.getDate() === 1) {
+          return true;
+        }
+        return false;
+      },
+      startDate: addYears(-20),
+      endDate: addYears(2)
+    });
+  });
+
+  function addYears(num){
+  var currYear = new Date().getFullYear();
+  var currMonth = new Date().getMonth();
+  return new Date(currYear+num, currMonth, 1);
+}
 </script>

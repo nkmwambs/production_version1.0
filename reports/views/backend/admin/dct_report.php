5cf4682d277fe8571852df60dc79a46529547e53
Nicodemus Karisa <nkarisa@ke.ci.org>
2020-12-01 14:03:10 +0300

    Only show first day of the month

--- reports/views/backend/admin/dct_report.php
+++ reports/views/backend/admin/dct_report.php
@@ -111,5 +111,26 @@
         });
 
     });
+
+
+$(document).ready(function(){
+  $('.datepicker').datepicker({
+			format: 'yyyy-mm-dd',
+      beforeShowDay: function(d){
+        if( d.getDate() === 1 ){
+          return true;
+        }
+        return false;
+      },
+      startDate: addYears(-10),
+      endDate: addYears(2)
+		});
+});
+
+function addYears(num){
+  var currYear = new Date().getFullYear();
+  var currMonth = new Date().getMonth();
+  return new Date(currYear+num, currMonth, 1);
+}
 </script>
 

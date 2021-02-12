1a606656c09944a1fd6f18205b8f179ec075ed7b
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-02-12 10:55:04 +0300

    Increase time limit for Accountant loading dashboard - 2

--- ifms/controllers/Accountant.php
+++ ifms/controllers/Accountant.php
@@ -14,6 +14,8 @@ class Accountant extends CI_Controller
 		
 		$this->load->library('finance_dashboard');
 		$this->load->model('dashboard_model');
+
+		set_time_limit(5000);
 			
        /*cache control*/
 		// $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');

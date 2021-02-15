39116a520887657ffc5ef3f8731b2b81a7e9ecc0
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-02-15 17:59:33 +0300

    Add href and onclick events to li elements in finance dashboard - 3

--- ifms/helpers/ui_helper.php
+++ ifms/helpers/ui_helper.php
@@ -54,11 +54,8 @@ if(!function_exists('fcp_reports_dropdown')){
 
         foreach($label_array as $label => $url_components){
             $target = $url_components['href'] != '#'?"target='__blank'":'';
-            // $li .= "<li>
-            // <a ".$target." href= '".$url_components['href']."' onclick = '".$url_components['onclick']."'>".get_phrase($label)."</a>
-            // </li>";
-
-            $li .= "<li><a ".$target." href='".$url_components['href']."' onclick=''>".ucwords(str_replace('_',' ',$label))."</a></li>";
+          
+            $li .= "<li><a ".$target." href='".$url_components['href']."' onclick='".$url_components['onclick']."'>".ucwords(str_replace('_',' ',$label))."</a></li>";
             $li .= "<li class='divider'></li>";
         }
 

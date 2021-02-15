5de02eb63524accc8631a2cdef96f905232b8152
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-02-15 15:26:43 +0300

    Start removing entypo font in code

--- ifms/views/backend/includes_top.php
+++ ifms/views/backend/includes_top.php
@@ -37,7 +37,7 @@
 
 
 <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
-<link type="text/css" rel="stylesheet" href="<?php echo base_url();?>assets/css/font-icons/entypo/css/entypo.css">
+<!-- <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>assets/css/font-icons/entypo/css/entypo.css"> -->
 <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic">
 <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>assets/css/neon-core.css">
 <link type="text/css" rel="stylesheet" href="<?php echo base_url();?>assets/css/neon-theme.css">

1998c0427fa2b4a207e39bd095233043a7245881
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-02-15 18:06:17 +0300

    Working on the onclick events in finance dashboard - 2

--- ifms/helpers/ui_helper.php
+++ ifms/helpers/ui_helper.php
@@ -24,7 +24,7 @@ if(!function_exists('fcp_reports_dropdown')){
             ],
             'bank_reconciliation' => [
                 'href'=> "#",
-                'onclick' => "showAjaxModal(".base_url()."ifms.php/modal/popup/modal_bank_reconcile/".date('Y-m-01',$month)."/".$fcp_id.")"
+                'onclick' => "showAjaxModal('".base_url()."ifms.php/modal/popup/modal_bank_reconcile/".date('Y-m-01',$month)."/".$fcp_id."')"
 
                // '".base_url()."ifms.php/modal/popup/modal_bank_reconcile/".date('Y-m-01',$month)."/".$fcp_id."'
             ],

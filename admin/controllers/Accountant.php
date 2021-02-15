8e86a3e44fb035941448f90ec742895013096fe8
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-02-15 17:43:55 +0300

    Announcement code update - 2

--- admin/controllers/Accountant.php
+++ admin/controllers/Accountant.php
@@ -39,7 +39,8 @@ class Accountant extends CI_Controller
     {
         if ($this->session->userdata('admin_login') != 1)
             redirect(base_url(), 'refresh');
-			
+		
+		$page_data['active_announcements'] = $this->get_active_announcements();	
         $page_data['page_name']  = 'dashboard';
         $page_data['page_title'] = get_phrase('apps_login_panel');
         $this->load->view('backend/index', $page_data);

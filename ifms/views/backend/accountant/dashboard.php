41f3935b8af7a4b77d5a23f8f5198bd08a045003
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-02-15 14:59:12 +0300

    Check if grid array in finance dashboard exhausts resources

--- ifms/views/backend/accountant/dashboard.php
+++ ifms/views/backend/accountant/dashboard.php
@@ -1,3 +1,13 @@
 <?php 
-print_r($none_requested_params);
+
+$grid_array = $this->dashboard_model->display_dashboard(date('Y-m-t',$month));
+
+$none_requested_params = isset($grid_array['parameters']['no']) ? $grid_array['parameters']['no'] : array();
+
+$requested_params = isset($grid_array['parameters']['yes']) ? $grid_array['parameters']['yes'] : array();
+
+print_r($grid_array);
+
 ?>
+
+<hr/>

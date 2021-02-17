6956ac8c5fea2bdab9755285f570e39df7a9abe7
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-02-17 14:39:05 +0300

    Testing use of FCPATH when uploading claim receipts

--- claims/controllers/Partner.php
+++ claims/controllers/Partner.php
@@ -436,7 +436,7 @@ class Partner extends CI_Controller
 
 	        if($this->input->post('fileSubmit') && !empty($_FILES['userFiles']['name'])){
 	        	
-							$uploadPath = 'uploads/document/medical/'.$param1;
+							$uploadPath = FCPATH.'uploads/document/medical/'.$param1;
 							//Check if group folder exists
 							if(!file_exists($uploadPath."/".$this->input->post('claim_id'))){
 								mkdir($uploadPath."/".$this->input->post('claim_id'));

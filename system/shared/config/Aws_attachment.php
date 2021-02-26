7326b20aa5f33e060a4de955e6b74f05d2bcfd6d
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-02-26 20:45:27 +0300

    Reload statement page after upload

--- system/shared/config/Aws_attachment.php
+++ system/shared/config/Aws_attachment.php
@@ -7,6 +7,8 @@
 	$config['temp_files_deletion_limit_hours'] = 0.5; // In hours. Use fractional of while or whole number e.g. 1, 2, 2.5
 
     $config['attachment_table_name'] = 'attachment';
+    $config['write_db_array_connection_key'] = 'db';
+    $config['read_db_array_connection_key'] = 'db';
     $config['attachment_key_column'] = 'attachment_primary_id'; 
 	$config['encrypt_file'] = false;
 

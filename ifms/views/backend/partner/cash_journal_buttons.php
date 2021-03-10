47509ae55f462c8eb8b4a2043c4060bd554255b7
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-03-10 17:06:51 +0300

    Completed FCP expense report optimization

--- ifms/views/backend/partner/cash_journal_buttons.php
+++ ifms/views/backend/partner/cash_journal_buttons.php
@@ -1,5 +1,5 @@
 <a href="#" class="btn btn-default" onclick="PrintElem('#balance_summary');"><i class="fa fa-print"></i><?=get_phrase('print');?></a>
 							
-							<div class="btn btn-default" id="toggleColumns"><?=get_phrase('show_spread');?></div>
+<div class="btn btn-default" id="toggleColumns"><?=get_phrase('show_spread');?></div>
 								
-							<div class="btn btn-default not-selected" id="chkAll"><?=get_phrase('select_vouchers');?></div>
+<div class="btn btn-default not-selected" id="chkAll"><?=get_phrase('select_vouchers');?></div>
